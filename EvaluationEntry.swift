5a2c467ef2bf29fb0d5f5ca95e9ab27988a3cc2a
Ravi_IOS <ravi.dwivedi@adglobal360.com>
2016-10-05 15:39:14 +0530

    added fiels

---
+++ EvaluationEntry.swift
@@ -0,0 +1,10 @@
+//Ravi shahtri
+import Foundation
+import CoreData
+
+
+class FuelType: NSManagedObject {
+    
+    // Insert code here to add functionality to your managed object subclass
+    
+}
